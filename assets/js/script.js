ad3d1c15c92c47361ba4e448e60b63391840d3c4
Meenadshi <88670187+Meenadshi@users.noreply.github.com>
2024-02-06 12:40:08 +0530

    First Commit with project source code

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var clickButton = document.getElementById('clickButton');
+
+    clickButton.addEventListener('click', function() {
+        alert('Hello, World');
+    });
+});
